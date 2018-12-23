3ae74abd24075b6d0c1ffe50aecb38eff0a57846
Sammer95 <merante.s@hotmail.com>
2018-12-22 23:10:31 -0500

    Added starting nav menu

--- sam-merante/src/app/app-routing.module.ts
+++ sam-merante/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TestComponent } from './test/test.component';
 
 const routes: Routes = [
-  { path: 'test', component: TestComponent },
   { path: 'home', redirectTo: '/' },
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '*', redirectTo: '/' }

0593dbc298a5e4ff92a00ee064696ed28217e40b
Sammer95 <merante.s@hotmail.com>
2018-12-23 15:19:19 -0500

    Updated site with cv

--- sam-merante/src/app/app-routing.module.ts
+++ sam-merante/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CVComponent } from './cv/cv.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent},
+  { path: 'cv', component: CVComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '*', redirectTo: '/home' }
 ];

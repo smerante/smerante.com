bcb1b84a4396702ce8b2c6651fccbda74df1aaf6
Sammer95 <merante.s@hotmail.com>
2018-12-22 20:30:51 -0500

    Routing test

--- sam-merante/src/app/app-routing.module.ts
+++ sam-merante/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'test', component: TestComponent },
+  { path: 'home', redirectTo: '/' },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '*', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

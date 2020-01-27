792df70e06bd05df4540a1607bf412b66bf7fefb
Sammer95 <merante.s@hotmail.com>
2020-01-26 20:42:37 -0500

    feat: transfered java to js functionality

--- sam-merante/src/app/lotto/lotto.component.ts
+++ sam-merante/src/app/lotto/lotto.component.ts
@@ -58,21 +58,129 @@ export class LottoComponent implements OnInit {
         winningNumbers[i].winningNumbers[j] = parseInt(winningNumbers[i].winningNumbers[j], 10);
       }
     }
-    console.warn(winningNumbers);
+    console.warn("predicting possible 5 sequences...");
+    this.predictNextUniqueNumbers(winningNumbers, 3, 49, 1);
+    this.predictNextUniqueNumbers(winningNumbers, 3, 49, 1);
+    this.predictOffPreviousWinningNum(winningNumbers, 2, 49, 1);
+    this.predictOffPreviousWinningNum(winningNumbers, 2, 49, 1);
+    this.predictOffPreviousWinningNum(winningNumbers, 3, 49, 1);
   }
 
-  // for (int i = 1; i < rows; i++) {
-  // 	XSSFRow row = myExcelSheet.getRow(i);
-  // 	ArrayList<String> data = stripNonDigits(row.getCell(2).getStringCellValue());
-  // 	for (int j = 0; j < data.size(); j++) {
-  // 		try {
-  // 			winningNumbers[i - 1][j] = Integer.parseInt(data.get(j));
-  // 		} catch (Exception e) {
-  // 			System.out.println(e);
-  // 			System.out.println(
-  // 					"week " + (i + 1) + ", seq: " + data + " returned an error: \"" + data.get(j) + "\"");
-  // 		}
-  // 	}
-  // }
 
+  predictNextUniqueNumbers(winningNumbers: WinningNumbers[], similarities: number, high: number, low: number) {
+    let newSequence: number[] = [];
+
+    newSequence = this.generateNewSequence(high, low);
+
+    let previouslyDrawn: boolean = true;
+
+    while (previouslyDrawn) {
+      let maxSimilarNumbers: number = 0;
+
+      // WeekI
+      for (let weekI = 0; weekI < winningNumbers.length; weekI++) {
+        let similarNumbersForRow: number = 0;
+
+        // For each number drawn that week
+        for (let numI = 0; numI < winningNumbers[weekI]['winningNumbers'].length; numI++) {
+
+          // Check every generated number
+          for (let newGI = 0; newGI < 7; newGI++) {
+            let currentNumber = newSequence[newGI];
+            if (currentNumber == winningNumbers[weekI][numI]) {
+              similarNumbersForRow++;
+            }
+          }
+        }
+        maxSimilarNumbers = similarNumbersForRow > maxSimilarNumbers ? similarNumbersForRow : maxSimilarNumbers;
+      }
+
+      if (maxSimilarNumbers <= similarities) {
+        previouslyDrawn = false;
+        newSequence = newSequence.sort()
+        console.warn('\t', newSequence, ' + [' + Math.round(Math.random() * 49 + 1) + ']');
+      } else {
+        newSequence = this.generateNewSequence(high, low);
+      }
+    }
+  }
+
+  predictOffPreviousWinningNum(winningNumbers: WinningNumbers[], simToPrevWeek: number, high: number, low: number) {
+    let newSequence = [];
+
+    newSequence = this.generateNewSequence(high, low);
+
+    let previouslyDrawn = true;
+
+    while (previouslyDrawn) {
+      let maxSimilarNumbers = 0;
+      let maxSimilarNumbersToPrevWeek = 0;
+      let lastDrawnWeek = 0;
+      let simToLastDrawn = 0;
+      // Check numbers for last drawn winning numbers
+      for (let numI = 0; numI < 7; numI++) {
+
+        // Check every generated number
+        for (let newGI = 0; newGI < 7; newGI++) {
+          let currentNumber = newSequence[newGI];
+          if (currentNumber == winningNumbers[lastDrawnWeek]['winningNumbers'][numI]) {
+            simToLastDrawn++;
+          }
+        }
+      }
+      maxSimilarNumbersToPrevWeek = simToLastDrawn > maxSimilarNumbersToPrevWeek ? simToLastDrawn
+        : maxSimilarNumbersToPrevWeek;
+
+      if (maxSimilarNumbersToPrevWeek == simToPrevWeek) {
+
+        // WeekI
+        for (let weekI = 0; weekI < winningNumbers.length; weekI++) {
+          let similarNumbersForRow = 0;
+
+          // For each number drawn that week
+          for (let numI = 0; numI < winningNumbers[weekI]['winningNumbers'].length; numI++) {
+
+            // Check every generated number
+            for (let newGI = 0; newGI < 7; newGI++) {
+              let currentNumber = newSequence[newGI];
+              if (currentNumber == winningNumbers[weekI]['winningNumbers'][numI]) {
+                similarNumbersForRow++;
+              }
+            }
+          }
+
+          maxSimilarNumbers = similarNumbersForRow > maxSimilarNumbers ? similarNumbersForRow
+            : maxSimilarNumbers;
+        }
+        if (maxSimilarNumbers <= 3) {
+          previouslyDrawn = false;
+          newSequence = newSequence.sort()
+          console.warn('\t', newSequence, ' + [' + Math.round(Math.random() * 49 + 1) + ']');
+        } else
+          newSequence = this.generateNewSequence(high, low);
+
+      } else
+        newSequence = this.generateNewSequence(high, low);
+
+    }
+
+  }
+
+  generateNewSequence(high: number, low: number): number[] {
+    let newSequence: number[] = []
+    let randomNumber: number[] = [];
+
+    for (let i = 0; i < 7; i++) {
+      let randomDigit: number = Math.round(Math.random() * high + low);
+
+      while (randomNumber.includes(randomDigit)) {
+        randomDigit = Math.round(Math.random() * high + low);
+      }
+
+      randomNumber.push(randomDigit);
+      newSequence[i] = randomDigit;
+    }
+
+    return newSequence;
+  }
 }

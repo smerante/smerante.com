438e52e8a76f678d9854471e70c6c20d3da4ed57
smerante <merante.s@hotmail.com>
2020-11-15 10:12:24 -0500

    feat: update to ng11

--- sam-merante/src/app/components/radios/radios.component.spec.ts
+++ sam-merante/src/app/components/radios/radios.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RadiosComponent } from './radios.component';
 
@@ -6,7 +6,7 @@ describe('RadiosComponent', () => {
   let component: RadiosComponent;
   let fixture: ComponentFixture<RadiosComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RadiosComponent ]
     })

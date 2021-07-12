a34ed4f749624867da02a52b63a37e80cf6d2e35
smerante <merante.s@hotmail.com>
2021-07-12 00:00:38 -0400

    feat: highcharts basic example

--- smerante/src/App.tsx
+++ smerante/src/App.tsx
@@ -8,8 +8,39 @@ import {
 import './App.scss';
 import HomeComponent from './pages/home/Home';
 import NavComponent from './components/nav/Nav';
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
 
 function App() {
+
+  const highChartsOptions = {
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Fruit Consumption'
+    },
+    xAxis: {
+      title: {
+        text: 'Fruit'
+      },
+      categories: ['Apples', 'Bananas', 'Oranges']
+    },
+    yAxis: {
+      title: {
+        text: 'Fruit eaten'
+      },
+      categories: [0, 1, 2, 3, 4, 5, 6, 7]
+    },
+    series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+    }, {
+      name: 'John',
+      data: [5, 7, 3]
+    }]
+  }
+
   return (
     <Router>
       <NavComponent />
@@ -17,6 +48,10 @@ function App() {
         <Switch>
           <Route exact path={["/", "/home"]}>
             <HomeComponent></HomeComponent>
+            <HighchartsReact
+              highcharts={Highcharts}
+              options={highChartsOptions}
+            />
           </Route>
           <Route exact path={["/cv"]}>
             <CVComponent></CVComponent>
